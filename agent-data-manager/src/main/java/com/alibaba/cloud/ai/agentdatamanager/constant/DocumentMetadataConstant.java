1a1bc6e5526d834b9e88f9e133e669a88d6f9f1f
48476 <484764968@qq.com>
2026-04-14 15:13:50 +0800

    feat: add core agent configuration

---
+++ agent-data-manager/src/main/java/com/alibaba/cloud/ai/agentdatamanager/constant/DocumentMetadataConstant.java
@@ -0,0 +1,41 @@
+
+package com.alibaba.cloud.ai.agentdatamanager.constant;
+
+// Document中metadata中存储的key
+public final class DocumentMetadataConstant {
+
+	private DocumentMetadataConstant() {
+
+	}
+
+	// column
+	public static final String COLUMN = "column";
+
+	// table
+	public static final String TABLE = "table";
+
+	// name
+	public static final String NAME = "name";
+
+	// tableName
+	public static final String TABLE_NAME = "tableName";
+
+	// vectorType
+	public static final String VECTOR_TYPE = "vectorType";
+
+	// knowledgeId
+	public static final String DB_AGENT_KNOWLEDGE_ID = "agentKnowledgeId";
+
+	// FAQ/DOCUMENT/QA
+	public static final String CONCRETE_AGENT_KNOWLEDGE_TYPE = "concreteAgentKnowledgeType";
+
+	// 智能体的知识
+	public static final String AGENT_KNOWLEDGE = "agentKnowledge";
+
+	// businessTerm
+	public static final String BUSINESS_TERM = "businessTerm";
+
+	// businessTermId
+	public static final String DB_BUSINESS_TERM_ID = "businessTermId";
+
+}
